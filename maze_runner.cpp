6f3c42e2164ff7a9bc7d770a239798841b5a19e3
Pedro Rosa <83311545+pedrorosa11@users.noreply.github.com>
2023-08-30 13:37:27 +0000

    Pronto

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -55,68 +55,62 @@ void print_maze() {
 bool walk(pos_t pos) {
     valid_positions.push(pos);
 
-    
 
     while (!valid_positions.empty()) {
         pos_t current_position = valid_positions.top();
         valid_positions.pop();
 
-    if (maze[pos.i][pos.j] == 's') {
-        return true;
-    }
+         if (maze[current_position.i][current_position.j] == 's') {
+            return true;
+        }
 
         maze[current_position.i][current_position.j] = '.';
-
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
         system("clear");
         print_maze();
 
         pos_t new_position;
 
+
 //Verificar abaixo
 new_position.i = current_position.i + 1;
 new_position.j = current_position.j;
-if (new_position.i >= 0 && new_position.i < num_rows && new_position.j >= 0 && new_position.j < num_cols && maze[new_position.i][new_position.j] == 'x') {
-    if (maze[new_position.i][new_position.j] == 's') {
-        return true;
-    }
+if (new_position.i >= 0 && new_position.i < num_rows && new_position.j >= 0 && new_position.j < num_cols && maze[new_position.i][new_position.j] != '#' && maze[new_position.i][new_position.j] != '.') {
     valid_positions.push(new_position);
 }
 
 // Verificar acima
-pos_t new_position_above;
-new_position_above.i = current_position.i - 1;
-new_position_above.j = current_position.j;
-if (new_position_above.i >= 0 && new_position_above.i < num_rows && new_position_above.j >= 0 && new_position_above.j < num_cols && maze[new_position_above.i][new_position_above.j] == 'x') {
-    valid_positions.push(new_position_above);
-    if (maze[new_position_above.i][new_position_above.j] == 's') {
+pos_t new_position_acima;
+new_position_acima.i = current_position.i - 1;
+new_position_acima.j = current_position.j;
+if (new_position_acima.i >= 0 && new_position_acima.i < num_rows && new_position_acima.j >= 0 && new_position_acima.j < num_cols && maze[new_position_acima.i][new_position_acima.j] != '#' && maze[new_position_acima.i][new_position_acima.j] != '.') {
+    valid_positions.push(new_position_acima);
+    if (maze[new_position_acima.i][new_position_acima.j] == 's') {
         return true;
     }
 }
 
 // Verificar direita
-pos_t new_position_right;
-new_position_right.i = current_position.i;
-new_position_right.j = current_position.j + 1;
-if (new_position_right.i >= 0 && new_position_right.i < num_rows && new_position_right.j >= 0 && new_position_right.j < num_cols && maze[new_position_right.i][new_position_right.j] == 'x') {
-    valid_positions.push(new_position_right);
-    if (maze[new_position_right.i][new_position_right.j] == 's') {
+pos_t new_position_direita;
+new_position_direita.i = current_position.i;
+new_position_direita.j = current_position.j + 1;
+if (new_position_direita.i >= 0 && new_position_direita.i < num_rows && new_position_direita.j >= 0 && new_position_direita.j < num_cols && maze[new_position_direita.i][new_position_direita.j] != '#' && maze[new_position_direita.i][new_position_direita.j] != '.') {
+    valid_positions.push(new_position_direita);
+    if (maze[new_position_direita.i][new_position_direita.j] == 's') {
         return true;
     }
 }
 
 // Verificar esquerda
-pos_t new_position_left;
-new_position_left.i = current_position.i;
-new_position_left.j = current_position.j - 1;
-if (new_position_left.i >= 0 && new_position_left.i < num_rows && new_position_left.j >= 0 && new_position_left.j < num_cols && maze[new_position_left.i][new_position_left.j] == 'x') {
-    valid_positions.push(new_position_left);
-    if (maze[new_position_left.i][new_position_left.j] == 's') {
+pos_t new_position_esquerda;
+new_position_esquerda.i = current_position.i;
+new_position_esquerda.j = current_position.j - 1;
+if (new_position_esquerda.i >= 0 && new_position_esquerda.i < num_rows && new_position_esquerda.j >= 0 && new_position_esquerda.j < num_cols && maze[new_position_esquerda.i][new_position_esquerda.j] != '#' && maze[new_position_esquerda.i][new_position_esquerda.j] != '.') {
+    valid_positions.push(new_position_esquerda);
+    if (maze[new_position_esquerda.i][new_position_esquerda.j] == 's') {
         return true;
     }
 }
-
 
     }
 //Verifica se a pilha de posições não está vazia
@@ -133,7 +127,7 @@ return walk(next_position);
 
 int main() {
 
-    pos_t initial_pos = load_maze("../data/maze.txt");   
+    pos_t initial_pos = load_maze("../data/maze5.txt");   
     print_maze();
     bool exit_found = walk(initial_pos);
     
