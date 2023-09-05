b46a4bf73f10173a6a3af33ed9e18451b2ed4628
Pedro Rosa <83311545+pedrorosa11@users.noreply.github.com>
2023-09-05 23:34:47 +0000

    ta

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -2,6 +2,8 @@
 #include <stack>
 #include <cstdlib>  // Adicionado para usar a função system("clear")
 #include <thread>
+#include <vector>
+#include <mutex>
 
 // Matriz de char representando o labirinto
 char** maze;
@@ -17,6 +19,7 @@ struct pos_t {
 };
 
 std::stack<pos_t> valid_positions;
+std::mutex stack_mutex;
 
 pos_t load_maze(const char* file_name) {
     pos_t initial_pos;
@@ -55,12 +58,11 @@ void print_maze() {
 bool walk(pos_t pos) {
     valid_positions.push(pos);
 
-
     while (!valid_positions.empty()) {
         pos_t current_position = valid_positions.top();
         valid_positions.pop();
 
-         if (maze[current_position.i][current_position.j] == 's') {
+        if (maze[current_position.i][current_position.j] == 's') {
             return true;
         }
 
@@ -69,61 +71,55 @@ bool walk(pos_t pos) {
         system("clear");
         print_maze();
 
+        std::vector<std::thread> threads_to_create;
+
+        // Verificar abaixo
         pos_t new_position;
+        new_position.i = current_position.i + 1;
+        new_position.j = current_position.j;
+        if (new_position.i >= 0 && new_position.i < num_rows && new_position.j >= 0 && new_position.j < num_cols && maze[new_position.i][new_position.j] != '#' && maze[new_position.i][new_position.j] != '.') {
+            threads_to_create.emplace_back(walk, new_position);
+        }
 
+        // Verificar acima
+        pos_t new_position_acima;
+        new_position_acima.i = current_position.i - 1;
+        new_position_acima.j = current_position.j;
+        if (new_position_acima.i >= 0 && new_position_acima.i < num_rows && new_position_acima.j >= 0 && new_position_acima.j < num_cols && maze[new_position_acima.i][new_position_acima.j] != '#' && maze[new_position_acima.i][new_position_acima.j] != '.') {
+            threads_to_create.emplace_back(walk, new_position_acima);
+        }
 
-//Verificar abaixo
-new_position.i = current_position.i + 1;
-new_position.j = current_position.j;
-if (new_position.i >= 0 && new_position.i < num_rows && new_position.j >= 0 && new_position.j < num_cols && maze[new_position.i][new_position.j] != '#' && maze[new_position.i][new_position.j] != '.') {
-    valid_positions.push(new_position);
-}
+        // Verificar direita
+        pos_t new_position_direita;
+        new_position_direita.i = current_position.i;
+        new_position_direita.j = current_position.j + 1;
+        if (new_position_direita.i >= 0 && new_position_direita.i < num_rows && new_position_direita.j >= 0 && new_position_direita.j < num_cols && maze[new_position_direita.i][new_position_direita.j] != '#' && maze[new_position_direita.i][new_position_direita.j] != '.') {
+            threads_to_create.emplace_back(walk, new_position_direita);
+        }
 
-// Verificar acima
-pos_t new_position_acima;
-new_position_acima.i = current_position.i - 1;
-new_position_acima.j = current_position.j;
-if (new_position_acima.i >= 0 && new_position_acima.i < num_rows && new_position_acima.j >= 0 && new_position_acima.j < num_cols && maze[new_position_acima.i][new_position_acima.j] != '#' && maze[new_position_acima.i][new_position_acima.j] != '.') {
-    valid_positions.push(new_position_acima);
-    if (maze[new_position_acima.i][new_position_acima.j] == 's') {
-        return true;
+        // Verificar esquerda
+        pos_t new_position_esquerda;
+        new_position_esquerda.i = current_position.i;
+        new_position_esquerda.j = current_position.j - 1;
+        if (new_position_esquerda.i >= 0 && new_position_esquerda.i < num_rows && new_position_esquerda.j >= 0 && new_position_esquerda.j < num_cols && maze[new_position_esquerda.i][new_position_esquerda.j] != '#' && maze[new_position_esquerda.i][new_position_esquerda.j] != '.') {
+            threads_to_create.emplace_back(walk, new_position_esquerda);
+        }
+
+        // Criar threads adicionais
+        stack_mutex.lock();
+        while (threads_to_create.size() > 1) {
+            threads_to_create.back().detach();
+            threads_to_create.pop_back();
+        }
+        if (!threads_to_create.empty()) {
+            threads_to_create.back().swap(threads_to_create[0]);
+            threads_to_create.pop_back();
+        }
+        stack_mutex.unlock();
     }
-}
-
-// Verificar direita
-pos_t new_position_direita;
-new_position_direita.i = current_position.i;
-new_position_direita.j = current_position.j + 1;
-if (new_position_direita.i >= 0 && new_position_direita.i < num_rows && new_position_direita.j >= 0 && new_position_direita.j < num_cols && maze[new_position_direita.i][new_position_direita.j] != '#' && maze[new_position_direita.i][new_position_direita.j] != '.') {
-    valid_positions.push(new_position_direita);
-    if (maze[new_position_direita.i][new_position_direita.j] == 's') {
-        return true;
-    }
-}
-
-// Verificar esquerda
-pos_t new_position_esquerda;
-new_position_esquerda.i = current_position.i;
-new_position_esquerda.j = current_position.j - 1;
-if (new_position_esquerda.i >= 0 && new_position_esquerda.i < num_rows && new_position_esquerda.j >= 0 && new_position_esquerda.j < num_cols && maze[new_position_esquerda.i][new_position_esquerda.j] != '#' && maze[new_position_esquerda.i][new_position_esquerda.j] != '.') {
-    valid_positions.push(new_position_esquerda);
-    if (maze[new_position_esquerda.i][new_position_esquerda.j] == 's') {
-        return true;
-    }
-}
-
-    }
-//Verifica se a pilha de posições não está vazia
-if (!valid_positions.empty()){
-pos_t next_position = valid_positions.top();
-valid_positions.pop();
-return walk(next_position);
-
-}
 
     return false;
 }
-
 
 int main() {
 
